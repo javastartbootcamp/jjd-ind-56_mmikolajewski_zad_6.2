1df496d5a1a1b101da0c9764d2e7a005f031d521
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2022-12-19 11:53:12 +0000

    Rozwiązanie

---
+++ src/main/java/pl/javastart/task/ArrayUtils.java
@@ -0,0 +1,29 @@
+package pl.javastart.task;
+
+import java.util.Scanner;
+
+public class ArrayUtils {
+    public int[] createArray() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj rozmiar tablicy");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Podaj 5 liczb");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    private int sum = 0;
+
+    public int sumPow(int[] array) {
+        for (int row : array) {
+            sum += (int) Math.pow(row, 2);
+        }
+        return sum;
+    }
+}
